8ebb8922f5a2ea9fee411438aba238d5b648c97d
xReflake <79257338+xReflake@users.noreply.github.com>
2021-06-16 01:49:46 +0300

    Add Class

--- PythonTest.py
+++ PythonTest.py
@@ -1,12 +1,14 @@
-def test_fib(n):
-   a, b = 0.0, 1.0
-   for i in range(n):
-      a, b = a + b, a
-   print (a)
+import time
+class bench_cython:
+    def fib(self, n):
+        a, b = 0.0, 1.0
+        for i in range(n):
+            a, b = a + b, a
+            print (a)
 
-def integrate_f(a, b, N):
-    s = 0
-    dx = (b - a) / N
-    for i in range(N):
-        s += (a + i * dx) ** 2 - (a + i * dx)
-    return s * dx
+    def integrate_f(self, a, b, N):
+        s = 0
+        dx = (b - a) / N
+        for i in range(N):
+            s += (a + i * dx) ** 2 - (a + i * dx)
+        return s * dx
